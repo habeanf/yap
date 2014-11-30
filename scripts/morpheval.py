c368e23b02a557a50939512b9d5b2fe7ba102e89
Amir More <habeanf@gmail.com>
2014-11-30 16:34:46 +0200

    Add exact match to morph eval script

--- scripts/morpheval.py
+++ scripts/morpheval.py
@@ -5,6 +5,7 @@ import re
 import operator
 from itertools import chain
 from pprint import pprint
+from collections import Counter
 
 morph_funcs = [
    ['seg', [2,-1]],
@@ -60,15 +61,17 @@ def get_set_elements(sent, morph_func):
 if __name__ == '__main__':
    file_output = list(enumerate(depio.depread(sys.argv[1])))
    file_ref = list(enumerate(depio.depread(sys.argv[2])))
-   print '\t'.join(['comparison-function', 'gold', 'pred', 'true-positive', 'precision', 'recall', 'f1'])
+   print '\t'.join(['comparison-function', 'gold', 'pred', 'true-positive', 'precision', 'recall', 'f1', 'em'])
    for morph_func_name, morph_func_opts in morph_funcs:
       morph_func = get_morph_func(morph_func_name)
       output_set = set(flatten(map(lambda s: get_set_elements(s, morph_func), file_output)))
       ref_set = set(flatten(map(lambda s: get_set_elements(s, morph_func), file_ref)))
+      exact_match = Counter(map(lambda (x,y):x==y,zip(file_output, file_ref)))
+      exact_match_pct = float(exact_match[True])/float(len(file_ref))
       pred_segments = len(output_set)
       gold_segments = len(ref_set)
       true_positives = float(len(output_set.intersection(ref_set)))
       p = precision(true_positives, pred_segments)
       r = recall(true_positives, gold_segments)
       f1_score = f1(p, r)
-      print '\t'.join(map(str,[morph_func_name,gold_segments,pred_segments,int(true_positives),p,r,f1_score]))
+      print '\t'.join(map(str,[morph_func_name,gold_segments,pred_segments,int(true_positives),p,r,f1_score, exact_match_pct]))
