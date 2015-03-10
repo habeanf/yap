146e2eee57795e1b5645bde80c4a96f1d587a540
Amir More <habeanf@gmail.com>
2015-03-10 10:35:07 +0200

    Fix extractor for idle features

--- alg/transition/genericextractor.go
+++ alg/transition/genericextractor.go
@@ -365,7 +365,7 @@ func (x *GenericExtractor) Init() {
 
 func (x *GenericExtractor) Features(instance Instance, idle bool) []Feature {
 	conf, ok := instance.(Configuration)
-	idle = conf.Previous() != nil && (conf.GetLastTransition() == IDLE || conf.GetLastTransition() == x.POPTrans)
+	idle = conf.Previous() != nil && (conf.GetLastTransition() == IDLE || conf.GetLastTransition() == x.POPTrans || idle)
 	if !ok {
 		panic("Type assertion that instance is a Configuration failed")
 	}
