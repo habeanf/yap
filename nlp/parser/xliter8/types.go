1fa9a63cff8d58e0b7c1ce10f2341224161cf2a5
Amir More <habeanf@gmail.com>
2015-09-30 14:25:59 +0300

    Add quotation to transliteration

--- nlp/parser/xliter8/types.go
+++ nlp/parser/xliter8/types.go
@@ -11,8 +11,8 @@ type Interface interface {
 }
 
 const (
-	hebrew          string = "אבגדהוזחטיכלמנסעפצקרשת0123456789"
-	english         string = "ABGDHWZXJIKLMNSEPCQRFT0123456789"
+	hebrew          string = "אבגדהוזחטיכלמנסעפצקרשת0123456789\""
+	english         string = "ABGDHWZXJIKLMNSEPCQRFT0123456789U"
 	extra_heb       string = "ךםןץף"
 	extra_eng       string = "KMNCP"
 	heb_suffix_from string = "כמנפצ"
