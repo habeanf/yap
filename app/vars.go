46abc9d5cb9aad2584ebe1d8ba7be4fa5adfb390
Amir More <habeanf@gmail.com>
2015-12-09 14:12:23 +0200

    Add combination of minimum iteration requirements and convergence to morph eval

--- app/vars.go
+++ app/vars.go
@@ -488,8 +488,8 @@ func MakeMorphEvalStopCondition(instances []interface{}, goldInstances []interfa
 		if curResult == prevResult {
 			equalIterations += 1
 		}
-		// retval := curResult < prevResult || equalIterations > 2
-		retval := curIteration >= iterations
+		retval := (curIteration >= iterations) && (curResult < prevResult || equalIterations > 2)
+		// retval := curIteration >= iterations
 		log.Println("Result (F1): ", curResult, "Exact:", total.Exact, "TruePos:", total.TP, "in", total.Population, "POS F1:", curPosResult)
 		if retval {
 			log.Println("Stopping")
